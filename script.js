9881a701c2e9818570ec9b822d5c983ac3b865d9
Michal Roguski <michalrx@gmail.com>
2018-04-22 08:40:33 +0200

    Add class for person

---
+++ script.js
@@ -0,0 +1,9 @@
+var peopleDb = [];
+
+function Person(attributes) {
+    this.name = attributes.name;
+    this.surname = attributes.surname;
+    this.age = attributes.age;
+    this.role = attributes.role;
+};
+

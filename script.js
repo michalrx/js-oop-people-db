b0169defa4da16707e9fd88d1059c5b6596df731
Michal Roguski <michalrx@gmail.com>
2018-04-22 10:55:24 +0200

    Render class

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
-var personDb = [];
+//Class DB
+function PersonDb() {
+    this.database = [];
+}
 
+PersonDb.prototype.addPerson = function(person) {
+    this.database.push(person);
+}
+
+PersonDb.prototype.displayPeople = function() {
+    console.log(this.database);
+}
+
+//Class People
 function Person(attributes) {
     this.name = attributes.name;
     this.surname = attributes.surname;
     this.age = attributes.age;
     this.role = attributes.role;
-};
+}
+
+//Class for render methods
+function Render(attribute) {
+    this.container = document.getElementById(attribute);
+}
+
+Render.prototype.renderRow = function(name,surname,age,role) {
+    return '<div><div>' + name + '</div><div>' + surname + '</div><div>' + age + '</div><div>' + role + '</div>';
+}
+
+Render.prototype.setHtml = function(database) {
+    this.container.innerHTML = '';
+    var self = this;
+    dbInstance.database.map(function(person) {
+        self.container.innerHTML += this.renderRow(person.name, person.surname, person.age, person.role);
+    });
+}
+
+//Create object of PersonDb
+var dbInstance = new PersonDb();
+
+//Create object of Render
+var renderInstance = new Render('records');
+
+renderInstance.setHtml(dbInstance.database);
 
 function renderRow(name,surname,age,role) {
     return '<div><div>' + name + '</div><div>' + surname + '</div><div>' + age + '</div><div>' + role + '</div>'
@@ -14,7 +51,7 @@ function renderRow(name,surname,age,role) {
 function renderDb() {
     var containerDb = document.getElementById('records');
     containerDb.innerHTML = '';
-    personDb.map(function(person) {
+    dbInstance.database.map(function(person) {
         containerDb.innerHTML += renderRow(person.name, person.surname, person.age, person.role);
     });
 }
@@ -32,9 +69,8 @@ addButton.addEventListener('click', function() {
         age: inputAge,
         role: inputRole
     });
-    personDb.push(person1);
 
-    renderDb();
+    dbInstance.addPerson(person1);
+
+    renderInstance.setHtml(dbInstance.database);
 });
-
-renderDb();
